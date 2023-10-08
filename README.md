5f6d1c968c08e73bb3a27de9111d6a923a53699a
Felipe Leonardo <felipe.lb.mateus@gmail.com>
2023-10-08 15:58:16 -0300

    base criada

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # nodets-adocao
+Projeto feito para praticar MVC, NodeJS e TypeScript
+
+### Pré-requisitos globais:
+`npm i -g nodemon typescript ts-node`
+
+### Instalação
+`npm install`
+
+### Para rodar o projeto
+`npm run start-dev`
---
+++ src/controllers/pageController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express';
+
+export const home = (req: Request, res: Response) => {
+    res.send('Home do controller!')
+}
+
+export const dogs = (req: Request, res: Response) => {
+    
+}
+
+export const cats = (req: Request, res: Response) => {
+    
+}
+
+export const fish = (req: Request, res: Response) => {
+    
+}
