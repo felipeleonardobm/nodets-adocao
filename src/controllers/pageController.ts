8c0edb0a367bc2a09053366134854fbe224d973f
Felipe Leonardo <felipe.lb.mateus@gmail.com>
2023-10-08 16:54:48 -0300

    final project 1.0

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,17 +1,47 @@
 import { Request, Response } from 'express';
+import { createMenuObject } from '../helpers/createMenuObject';
+import { Pet } from '../models/Pet';
 
 export const home = (req: Request, res: Response) => {
-    res.send('Home do controller!')
+    res.render('pages/page', {
+        pets: Pet.getAll(),
+        menu: createMenuObject('all'),
+        banner: {
+            title: 'Todos os animais',
+            background: 'allanimals.jpg'
+        }
+    });
 }
 
 export const dogs = (req: Request, res: Response) => {
-    
+    res.render('pages/page', {
+        pets: Pet.getFromSpecies('dog'),
+        menu: createMenuObject('dog'),
+        banner: {
+            title: 'Cachorros',
+            background: 'banner_dog.jpg'
+        }
+    });
 }
 
 export const cats = (req: Request, res: Response) => {
-    
+    res.render('pages/page', {
+        pets: Pet.getFromSpecies('cat'),
+        menu: createMenuObject('cat'),
+        banner: {
+            title: 'Gatos',
+            background: 'banner_cat.jpg'
+        }
+    });
 }
 
 export const fish = (req: Request, res: Response) => {
-    
+    res.render('pages/page', {
+        pets: Pet.getFromSpecies('fish'),
+        menu: createMenuObject('fish'),
+        banner: {
+            title: 'Peixes',
+            background: 'banner_fish.jpg'
+        }
+    });
 }
--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,5 +1,18 @@
 import { Request, Response } from 'express';
+import { Pet } from '../models/Pet';
+import { createMenuObject } from '../helpers/createMenuObject';
 
 export const search = (req: Request, res: Response) => {
-    
+    let query: string = req.query.q as string;
+
+    if(!query) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('pages/page', {
+        query,
+        pets: Pet.getFromName(query),
+        menu: createMenuObject(''),
+    })
 }
